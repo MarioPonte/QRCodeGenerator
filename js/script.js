fd66bf17c17fbfce30de4d671a18aa0ed2749529
Mário Ponte <ponteolavo30@gmail.com>
2022-09-22 20:16:35 +0100

    edit

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ const qr = document.getElementById("qrcode");
 const onGenerateSubmit = (e) => {
     e.preventDefault();
 
+    clearUI();
+
     const url = document.getElementById("url").value;
     const size = document.getElementById("size").value;
 
@@ -11,21 +13,55 @@ const onGenerateSubmit = (e) => {
         alert("Introduza um URL");
     }else{
         // showSpinner();
+
+        setTimeout( () => {
+            //hideSpinner();
+            generateQRCode(url, size);
+
+            setTimeout( () => {
+                const saveUrl = qr.querySelector("img").src;
+                createSaveBtn(saveUrl);
+            }, 50);
+        }, 1000);
     }
 
-    /*
+}
 
-    const showSpinner = () => {
-        document.getElementById("spinner").style.display = "block";
-    }
+const generateQRCode = (url, size) => {
+    const qrcode = new QRCode('qrcode', {
+        text: url,
+        width: size,
+        height: size,
+    });
+}
 
-    const hideSpinner = () => {
-        document.getElementById("spinner").style.display = "none";
-    }
+/*
 
-    hideSpinner();
+const showSpinner = () => {
+    document.getElementById("spinner").style.display = "block";
+}
 
-    */
+const hideSpinner = () => {
+    document.getElementById("spinner").style.display = "none";
+}
+
+hideSpinner();
+
+*/
+
+const clearUI = () => {
+    qr.innerHTML = "";
+    const saveLink = document.getElementById("save-link");
+    if(saveLink) saveLink.remove();
+}
+
+const createSaveBtn = (saveUrl) => {
+    const link = document.createElement('a');
+    link.id = "save-link";
+    link.href = saveUrl;
+    link.download = "qrcode";
+    link.innerHTML = "Salvar Imagem";
+    document.getElementById("generated").appendChild(link);
 }
 
 form.addEventListener("submit", onGenerateSubmit);
